744b5abfdc7f8b36604eaf3db8dc357a8f6c7701
Jennifer Gaillard <jengaillard1@gmail.com>
2019-02-08 01:42:59 +0000

    Done.

--- index.js
+++ index.js
@@ -69,12 +69,48 @@ function displayFiveDayForecast(json) {
 }
 function createChart(json) {
   //Bonus: render temperature chart using five day forecast data and ChartJS
-const ctx = document.getElementById('WeatherChart').getContent('2d');
-var mtChart = new Chart(ctx, {
-  
+  const dateLabels = json.list.map((forecast) => forecast
+  .dt_txt);
+  const tempData = json.list.map((forecast) => forecast.main.temp);
+const ctx = document.getElementById('WeatherChart').getContext('2d');
+var myChart = new Chart(ctx, {
+  type: 'line',
+    data: {
+        labels: dateLabels,
+        datasets: [{
+            label: 'Temperature',
+            data: tempData,
+            backgroundColor: [
+                'rgba(255, 99, 132, 0.2)',
+                'rgba(54, 162, 235, 0.2)',
+                'rgba(255, 206, 86, 0.2)',
+                'rgba(75, 192, 192, 0.2)',
+                'rgba(153, 102, 255, 0.2)',
+                'rgba(255, 159, 64, 0.2)'
+            ],
+            borderColor: [
+                'rgba(255,99,132,1)',
+                'rgba(54, 162, 235, 1)',
+                'rgba(255, 206, 86, 1)',
+                'rgba(75, 192, 192, 1)',
+                'rgba(153, 102, 255, 1)',
+                'rgba(255, 159, 64, 1)'
+            ],
+            borderWidth: 1
+        }]
+    },
+    options: {
+        scales: {
+            yAxes: [{
+                ticks: {
+                    beginAtZero:true
+                }
+            }]
+        }
+    }
+});
 }
-
-
+  
 document.addEventListener('DOMContentLoaded', function() {
   //add event listener here for form submission
   document.addEventListener('submit', handleFormSubmit);
